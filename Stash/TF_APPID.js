2854a02503eabd0b96c9903107e020b06528c287
wya0 <34045732+wya0@users.noreply.github.com>
2023-09-29 13:54:07 +0800

    更新 TF_APPID.js

--- Stash/TF_APPID.js
+++ Stash/TF_APPID.js
@@ -1,15 +1,15 @@
-const reg2 = /^https:\/\/testflight\.apple\.com\/join\/(.*)/
-let appId = $persistentStore.read("APP_ID")
+const reg2 = ^https:\/\/testflight\.apple\.com\/join\/(.*);
+let appId = $persistentStore.read("APP_ID");
 if (!appId) {
-  appId = ""
+  appId = "";
 }
-let arr = appId.split(",")
-const id = reg2.exec($request.url)[1]
-arr.push(id)
-arr = unique(arr).filter((a) => a)
+let arr = appId.split(",");
+const id = reg2.exec($request.url)[1];
+arr.push(id);
+arr = unique(arr).filter((a) => a);
 if (arr.length > 0) {
-  appId = arr.join(",")
+  appId = arr.join(",");
 }
-$persistentStore.write(appId, "APP_ID")
-$notification.post("TestFlight自动加入", `已添加APP_ID: ${id}`, `当前ID: ${appId}`)
-$done({})
+$persistentStore.write(appId, "APP_ID");
+$notification.post("TestFlight自动加入", `已添加APP_ID: ${id}`, `当前ID: ${appId}`);
+$done({});

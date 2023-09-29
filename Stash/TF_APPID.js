2aa87e78fff4651b0e67d08ada90f88492180c5a
wya0 <34045732+wya0@users.noreply.github.com>
2023-09-29 18:17:32 +0800

    更新 TF_APPID.js

--- Stash/TF_APPID.js
+++ Stash/TF_APPID.js
@@ -4,12 +4,18 @@ if (!appId) {
   appId = "";
 }
 let arr = appId.split(",");
-const id = reg2.exec($request.url)[1];
-arr.push(id);
-arr = unique(arr).filter((a) => a);
-if (arr.length > 0) {
-  appId = arr.join(",");
+const idMatch = reg2.exec($request.url);
+if (idMatch) {
+  const id = idMatch[1];
+  arr.push(id);
+  arr = [...new Set(arr)].filter((a) => a); // 使用Set去重
+  if (arr.length > 0) {
+    appId = arr.join(",");
+  }
+  $persistentStore.write(appId, "APP_ID");
+  $notification.post("TestFlight自动加入", `已添加APP_ID: ${id}`, `当前ID: ${appId}`);
+  $done({});
+} else {
+  // 处理没有匹配的情况
+  // 可以根据具体需求进行适当的处理
 }
-$persistentStore.write(appId, "APP_ID");
-$notification.post("TestFlight自动加入", `已添加APP_ID: ${id}`, `当前ID: ${appId}`);
-$done({});
